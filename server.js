e8bdda2bf996a8b2328234bfa015ed6e20e9a3fc
slamaslam466 <slamaslam466@gmail.com>
2021-04-09 17:06:15 +0530

    fc

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express          = require('express');
 const bodyParser       = require('body-parser');
 const app              = express();
-const mongodb          = require('mongodb')
+// const mongodb          = require('mongodb')
 require('dotenv').config()
-const port=3001;
+const port=process.env.Port || 3001;
 const hostname="localhost"
+const MongoClient = require('mongodb').MongoClient;
+const uri=process.env.DBURL
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("mongo-demo").collection("users");
+  // collection.insertMany([{"name":"Aslam","mail":"slamaslam466@gmail.com","code":"001"},{"name":"Aslam","mail":"slamaslam466@gmail.com","code":"002"}])
+  // collection.updateOne({"code":"001"},{$set:{"work":"intern at guvi"}})
+  // collection.updateMany({},{$set:{"status":"Active"}})
+  //   collection.updateOne({"code":"002"},{$set:{"status":"Inactive"}})
+  //   collection.deleteOne({"status":"Inactive"})
+    // console.log(collection.find({"code":"001"}))
 
+  // perform actions on the collection object
+  // client.close();
+});
+app.get("/products", async(req,res) => {
+  client.connect(err => {
+    const collection = client.db("mongo-demo").collection("users");
+    if(collection.findOne({ "name": "Aslam" }))
+    console.log("True");
+    else
+    console.log("False");
+
+  });
+ res.status(200).json("hiiii")
+});
 
 app.listen(port,hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
